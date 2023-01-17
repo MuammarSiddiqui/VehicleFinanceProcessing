9d1f600c1b0e6fc2051eeb739a7a128537631e47
Muammar Siddiuqi <87115874+MuammarSiddiqui@users.noreply.github.com>
2023-01-17 16:57:56 +0500

    1-17

--- app/src/main/java/com/example/vehiclefinanceprocessing/LoginActivity.java
+++ app/src/main/java/com/example/vehiclefinanceprocessing/LoginActivity.java
@@ -2,17 +2,13 @@ package com.example.vehiclefinanceprocessing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity2 extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-
+        setContentView(R.layout.activity_login);
     }
 }
