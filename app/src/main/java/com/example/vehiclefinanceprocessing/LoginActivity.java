7c66693d7980112d3eeb7c48b7c30f81b8163b74
Muammar Siddiuqi <87115874+MuammarSiddiqui@users.noreply.github.com>
2023-01-18 13:01:49 +0500

    1-18

--- app/src/main/java/com/example/vehiclefinanceprocessing/LoginActivity.java
+++ app/src/main/java/com/example/vehiclefinanceprocessing/LoginActivity.java
@@ -1,22 +1,31 @@
 package com.example.vehiclefinanceprocessing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    Button signupbutton ,login;
+    Button signupbutton ,login,loginsubmit;
+    EditText email,password;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         signupbutton = findViewById(R.id.Signup);
         login = findViewById(R.id.login);
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.Password);
+        loginsubmit = findViewById(R.id.loginsubmit);
         signupbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,8 +42,42 @@ public class LoginActivity extends AppCompatActivity {
                 view.startAnimation(AnimationUtils.loadAnimation(LoginActivity.this, R.anim.myanim));
             }
         });
+
+        loginsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = email.getText().toString().trim();
+                String PasswordText = password.getText().toString().trim();
+                if (text.matches(emailPattern))
+                {
+                    password.setBackground( ContextCompat.getDrawable(LoginActivity.this, R.drawable.inputbordershape));
+                    email.setError(null);
+//                    Toast.makeText(LoginActivity.this,"valid email address",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+
+                    email.setBackground( ContextCompat.getDrawable(LoginActivity.this, R.drawable.errorborder));
+                    Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+                    email.setError("Enter a valid Email");
+                }
+                if (PasswordText.length() < 8)
+                {
+                    password.setBackground( ContextCompat.getDrawable(LoginActivity.this, R.drawable.errorborder));
+                    password.setError("Minimun 8 characters Requires");
+                    Toast.makeText(LoginActivity.this,"Enter a valid Password",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    password.setBackground( ContextCompat.getDrawable(LoginActivity.this, R.drawable.inputbordershape));
+//                    Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+                    password.setError(null);
+                }
+            }
+        });
     }
     public void logoclicked(View v){
         v.startAnimation(AnimationUtils.loadAnimation(LoginActivity.this, R.anim.myanim));
     }
+
 }
